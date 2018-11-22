d004e518c2cd2437c0664232f61f11f66252afe1
Tien-Yiao Hsu <tienyiah@gplogin2.gp.local>
2018-11-22 12:40:18 -0800

    fix init_30m

--- src/05_parallel_stanfit/config.jl
+++ src/05_parallel_stanfit/config.jl
@@ -1,23 +1,33 @@
 using Formatting
 
-model_name = "NCAR_2deg"
+# Check:
+#
+# model_name
+# nchains, num_samples, num_warmup
+# exp_name [contains initial condition status]
+# 
+# sbatch --job-name, --output, --cpus-per-task, --time, --array
+
+
+
+model_name = "NCAR_5deg"
 
 nchains     = 4
 num_samples = 1000
-num_warmup  = 200
+num_warmup  = 50
 
 #nchains     = 2
 #num_samples = 10
 #num_warmup  = 2
 
 
-exp_name = format("HMC_{}_c{:d}_s{:d}_w{:d}", model_name, nchains, num_samples, num_warmup)
+exp_name = format("HMC_{}_init-30m_c{:d}_s{:d}_w{:d}", model_name, nchains, num_samples, num_warmup)
 
 
 default_log_file = joinpath("/export/home/tienyiah/projects/SMARTSLAB", format("{}.log", exp_name))
-function writelog(job_id, fmt, params...; ending="\n", log_file=default_log_file)
+function writelog(lon_i, fmt, params...; ending="\n", log_file=default_log_file)
     open(log_file, "a") do f
-        write(f, format("[{}] ", job_id))
+        write(f, format("[lon_i = {}] ", lon_i))
         write(f, format(fmt, params...))
         write(f, ending)
     end
--- src/05_parallel_stanfit/submit.sh
+++ src/05_parallel_stanfit/submit.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-#SBATCH --job-name=SMARTSLAB-KT-5deg
-#SBATCH --output=5deg.log
+#SBATCH --job-name=SMARTSLAB-KT-2deg
+#SBATCH --output=2deg.log
 #
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --time=01:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --time=48:00:00
 #SBATCH --mem-per-cpu=2048
 #SBATCH --mail-user=tienyiah@uci.edu
 
 #
 ##SBATCH --array=1-1
-#SBATCH --array=2-3
+#SBATCH --array=1-73
 
 srun julia stanfit_KT_single_longitude.jl $SLURM_ARRAY_TASK_ID
