61724734a27cccf59a0660d87f664bc227e49765
xtt <meteorologytoday@gmail.com>
2018-11-25 16:20:29 -0800

    Making plots

---
+++ src/05_parallel_stanfit/plot_maps.jl
@@ -0,0 +1,122 @@
+using PyCall
+@pyimport matplotlib as mpl
+mpl.use("Agg")
+
+using PyPlot
+
+@pyimport mpl_toolkits.basemap as basemap
+@pyimport mpl_toolkits.basemap.cm as cm
+
+using NCDatasets
+using Formatting
+
+cvt = x -> convert(Array{Float64}, nomissing(x, NaN))
+
+
+# Reading data
+data_dir = normpath(joinpath( dirname(@__FILE__), "..", "..", "data"))
+img_dir  = normpath(joinpath( dirname(@__FILE__), "..", "..", "img"))
+nc_filename = joinpath(data_dir, "HMC_NCAR_5deg_init-omlmax_c4_s1000_w200.nc")
+#nc_filename = joinpath(data_dir, "HMC_NCAR_2deg_c4_s1000_w200.nc")
+
+ds = Dataset(nc_filename, "r")
+
+lon = cvt(ds["lon"][:])
+lat = cvt(ds["lat"][:])
+data_h = cvt(ds["h_mean"][:])
+data_Q = cvt(ds["Q_mean"][:])
+data_Td = cvt(ds["Td_mean"][:]) .- 273.15
+
+clevs_h  = collect(range(0; stop=200, length=11))
+clevs_Q  = collect(range(-100; stop=100, length=11))
+clevs_Td = collect(range(-10; stop=15, length=11))
+
+llon = repeat(lon ; outer=(1, length(lat)))
+llat = repeat(lat'; outer=(length(lon), 1))
+
+### plot Td ###
+println("Plotting Td")
+
+# setting maps
+filename = normpath(joinpath(
+    img_dir,
+    format("{}-maps-Td.png", splitext(basename(nc_filename))[1])
+))
+
+fig, ax = plt[:subplots](1, 1, figsize=(8, 6))
+
+lab_lons = collect(  0:60:360)
+lab_lats = collect(-90:30: 90)
+
+m = basemap.Basemap(ax=ax,  projection="merc",llcrnrlat=-80,urcrnrlat=80,
+         llcrnrlon=0,urcrnrlon=360,lat_ts=20,resolution="c",lon_0=180.0)
+m[:drawcoastlines](linewidth=1.5)
+#m[:fillcontinents](color="#888888", lake_color="aqua")
+m[:drawmeridians](lab_lons, labels=repeat([true, ], outer=(length(lab_lons),)))
+m[:drawparallels](lab_lats, labels=repeat([true, ], outer=(length(lab_lats),)))
+
+x, y = m(llon, llat)
+
+#cmap = plt[:get_cmap]("gist_ncar_r")
+#cmap = plt[:get_cmap]("GnBu")
+#cmap = plt[:get_cmap]("gnuplot2_r")
+cmap = plt[:get_cmap]("bwr")
+cs = m[:contourf](x, y, data_Td[:, :], clevs_Td; cmap=cmap, extend="both")
+cbar = m[:colorbar](cs, location="bottom", pad="8%", ticks=clevs_Td)
+cbar[:set_label]("Deep Ocean Temperature \$T_\\mathrm{d}\$ [\$ {}^\\circ\\mathrm{C}\$]")
+
+fig[:suptitle](format("File: {}", basename(nc_filename)))
+plt[:show]()
+fig[:savefig](filename, dpi=100)
+println("Output file:", filename)
+plt[:close](fig)
+
+### plot h and Q ###
+for i = 1:size(data_h)[3]
+    println("Plotting month ", i)
+    # setting maps
+    filename = normpath(joinpath(
+        img_dir,
+        format("{}-maps-{:02d}.png", splitext(basename(nc_filename))[1], i)
+    ))
+
+    fig, axes = plt[:subplots](1, 2, figsize=(12, 6))
+    mm = []
+
+    lab_lons = collect(  0:60:360)
+    lab_lats = collect(-90:30: 90)
+
+    for ax in axes
+        m = basemap.Basemap(ax=ax,  projection="merc",llcrnrlat=-80,urcrnrlat=80,
+             llcrnrlon=0,urcrnrlon=360,lat_ts=20,resolution="c",lon_0=180.0)
+        push!(mm, m)
+
+        m[:drawcoastlines](linewidth=1.5)
+        #m[:fillcontinents](color="#888888", lake_color="aqua")
+        m[:drawmeridians](lab_lons, labels=repeat([true, ], outer=(length(lab_lons),)))
+        m[:drawparallels](lab_lats, labels=repeat([true, ], outer=(length(lab_lats),)))
+        
+    end
+
+    x, y = mm[1](llon, llat)
+
+    cmap = plt[:get_cmap]("gist_ncar_r")
+    cmap = plt[:get_cmap]("GnBu")
+    cmap = plt[:get_cmap]("gnuplot2_r")
+    cs = mm[1][:contourf](x, y, data_h[:, :, i], clevs_h; cmap=cmap, extend="both")
+    cbar = mm[1][:colorbar](cs, location="bottom", pad="8%", ticks=clevs_h)
+    cbar[:set_label]("MLD [\$\\mathrm{m}\$]")
+
+    cmap = plt[:get_cmap]("Spectral_r")
+    cs = mm[2][:contourf](x, y, data_Q[:, :, i], clevs_Q; cmap=cmap, extend="both")
+    cbar = mm[2][:colorbar](cs, location="bottom", pad="8%", ticks=clevs_Q)
+    cbar[:set_label]("Q flux [\$\\mathrm{W} \\, \\mathrm{m}^{-2} \$]")
+
+    fig[:suptitle](format("File: {} \n Month: {:02d}", basename(nc_filename), i))
+
+
+    #plt[:show]()
+    fig[:savefig](filename, dpi=100)
+    println("Output file:", filename)
+    plt[:close](fig)
+end
---
+++ src/05_parallel_stanfit/plot_slice.jl
@@ -0,0 +1,143 @@
+using PyCall
+@pyimport matplotlib as mpl
+mpl.use("Agg")
+
+using PyPlot
+
+@pyimport mpl_toolkits.basemap as basemap
+@pyimport mpl_toolkits.basemap.cm as cm
+
+using NCDatasets
+using Formatting
+using Statistics: mean, std
+
+
+function nanmean(x; dims)
+    n = isnan.(x)
+
+    y = copy(x)
+    y[n] .= 0.0
+
+    return sum(y; dims=dims) ./ sum(1 .- n; dims=dims)
+end
+
+#=
+function nanmean(x; dims)
+    xdev_sq = x -
+    return sum(x; dims=dims) ./ sum(m; dims=dims)
+end
+
+=#
+
+cvt  = x -> convert(Array{Float64}, nomissing(x, NaN))
+stat = x -> (mean(x, dims=(1,))[1,:, :], std(x, dims=(1,))[1,:,:])
+
+# Reading data
+data_dir = normpath(joinpath( dirname(@__FILE__), "..", "..", "data"))
+img_dir  = normpath(joinpath( dirname(@__FILE__), "..", "..", "img"))
+nc_filename = joinpath(data_dir, "HMC_NCAR_5deg_init-omlmax_c4_s1000_w200.nc")
+complex_model_filename = joinpath(data_dir, "NCAR_CESM1-WACCM", "SMART_5deg_omlmax_Omon_CESM1-WACCM_piControl_r1i1p1_009601-029512.nc")
+
+ds = Dataset(nc_filename, "r")
+data_h  = ds["h_mean"][:] |> cvt
+data_Q  = ds["Q_mean"][:] |> cvt
+data_Td = (ds["Td_mean"][:] |> cvt) .- 273.15
+
+lat = ds["lat"][:] |> cvt
+lon = ds["lon"][:] |> cvt
+close(ds)
+
+lon_rng = range(1, step=1, stop=length(lon)) |> collect
+
+h_rng = [0, 200]
+Q_rng = [-100, 100]
+Td_rng = [-15, 25]
+
+lat_rng = [-90, 90]
+
+h_mean  = nanmean(data_h; dims=(1,))[1,:, :]
+Q_mean  = nanmean(data_Q; dims=(1,))[1,:, :]
+Td_mean = nanmean(data_Td; dims=(1,))[1,:]
+
+lab_lats = collect(-90:30: 90)
+
+### plot Td ###
+println("Plotting Td")
+# setting maps
+filename = normpath(joinpath(
+    img_dir,
+    format("{}-slice-Td.png", splitext(basename(nc_filename))[1])
+))
+
+fig, ax = plt[:subplots](1, 1, figsize=(12, 6))
+
+ax[:set_xticks](lab_lats)
+ax[:set_xlabel]("Latitude [\$\\mathrm{deg}\$]")
+ax[:set_ylabel]("Deep Ocean Temperature \$T_\\mathrm{d}\$ [\$ {}^\\circ\\mathrm{C}\$]")
+ax[:set_xlim](lat_rng)
+ax[:set_ylim](Td_rng)
+
+for j = 1:length(lon_rng)
+    ax[:plot](lat, data_Td[lon_rng[j], :])
+end
+ax[:plot](lat, Td_mean, color="k", linestyle="--", linewidth=2, label="mean")
+
+ax[:legend]()
+ax[:grid]()
+
+fig[:suptitle](format("File: {}", basename(nc_filename)))
+
+
+#plt[:show]()
+fig[:savefig](filename, dpi=100)
+println("Output file:", filename)
+plt[:close](fig)
+
+
+### plot h and Q ###
+for i = 1:size(data_h)[3]
+    println("Plotting month ", i)
+    # setting maps
+    filename = normpath(joinpath(
+        img_dir,
+        format("{}-slice-{:02d}.png", splitext(basename(nc_filename))[1], i)
+    ))
+
+    fig, axes = plt[:subplots](2, 1, figsize=(10, 6), sharex=true)
+
+    axes[2][:set_xticks](lab_lats)
+    axes[2][:set_xlabel]("Latitude [\$\\mathrm{deg}\$]")
+    
+    axes[1][:set_ylabel]("MLD [\$\\mathrm{m}\$]")
+    axes[2][:set_ylabel]("Q flux [\$\\mathrm{W} \\, \\mathrm{m}^{-2} \$]")
+
+   
+    axes[1][:set_xlim](lat_rng)
+    axes[1][:set_ylim](h_rng)
+    axes[2][:set_ylim](Q_rng)
+
+    axes[1][:invert_yaxis]()
+
+    println(size(data_h))
+    for j = 1:length(lon_rng)
+        axes[1][:plot](lat, data_h[lon_rng[j], :, i])
+        axes[2][:plot](lat, data_Q[lon_rng[j], :, i])
+    end
+ 
+    axes[1][:plot](lat, h_mean[:, i], color="k", linestyle="--", linewidth=2, label="mean")
+    axes[2][:plot](lat, Q_mean[:, i], color="k", linestyle="--", linewidth=2, label="mean")
+
+    axes[1][:legend]()
+    axes[2][:legend]()
+
+    axes[1][:grid]()
+    axes[2][:grid]()
+    
+    fig[:suptitle](format("File: {} \n Month: {:02d}", basename(nc_filename), i))
+
+
+    #plt[:show]()
+    fig[:savefig](filename, dpi=100)
+    println("Output file:", filename)
+    plt[:close](fig)
+end
